0283bc76fdcc3b69c2725b53957476ef37716cfc
Himanshu Sharma <himanshuindia2004@gmail.com>
2025-03-30 12:49:28 +0530

    Implement modes

--- server/routes/courseRouter.js
+++ server/routes/courseRouter.js
@@ -25,7 +25,7 @@ import {
 const router = express.Router();
 router.route("/course").post(isAuthenticated, createCourse);
 router.route("/search").get(isAuthenticated, searchCourse);
-router.route("/published-courses").get(isAuthenticated, getPublishedCourse);
+router.route("/published-courses").get(getPublishedCourse);
 router.route("/creator-course").get(isAuthenticated, getCreatorCourses);
 router
   .route("/course/allPurchased")
